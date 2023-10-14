0eec23068b9b630739312dfc14718aaef816dee8
NotSenchik13 <114508976+NotSenchik13@users.noreply.github.com>
2023-10-14 16:51:37 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 let burger = document.querySelector('.header__burger');
 
 let news = document.querySelector('.header__news');
@@ -17,3 +16,22 @@ burger.addEventListener('click', function() {
     }
 })
 
+
+function checkWindowSize() {
+  
+    if (window.innerWidth >= 768) {
+      // Если ширина окна больше или равна 768px (размер десктопа), показываем шапку
+      news.style.display = 'block';
+      login.style.display = 'block';
+    } else {
+      // Если ширина окна меньше 768px (размер мобильного устройства), скрываем шапку, если меню свернуто
+      if (!menu.classList.contains('visible')) {
+        news.style.display = 'none';
+        login.style.display = 'none';
+      }
+    }
+  }
+  
+  // Проверяем размер окна при загрузке страницы и при изменении размера окна
+  window.addEventListener('load', checkWindowSize);
+  window.addEventListener('resize', checkWindowSize);
